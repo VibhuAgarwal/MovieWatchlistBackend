487dd5905e9eab4543b9ef4ad69fef2038653223
Vibhor Agarwal <vibhor.x.agarwal@goindigo.in>
2025-12-17 12:33:30 +0530

    Login logout and JWT token generation and setting the token in httlpOnly cookie

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,5 +1,6 @@
 import { prisma } from "../config/db.js";
 import bcrypt from "bcryptjs";
+import { generateToken } from "./generateToken.js";
 
 const register = async (req, res)=>{
     const {name, email, password} = req.body;
@@ -26,6 +27,9 @@ const register = async (req, res)=>{
         }
      });
 
+    const token = generateToken(user.id, res);
+
+
      res.status(201).json({
         status: "success",
         data:{
@@ -33,9 +37,56 @@ const register = async (req, res)=>{
                 id: user.id,
                 name: name,
                 email: email
-            }
+            },
+            token,
         }
      })
 }
 
-export {register};
+
+const login = async(req,res)=>{
+    const {email, password} = req.body;
+
+    //Check if user exists in table
+    const user = await prisma.user.findUnique({
+        where: {email: email},
+    });
+
+     if(!user){
+        return res.status(401).json({error: "Invalid Email or password"})
+    }
+
+    //verify password
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if(!isPasswordValid){
+        return res.status(401).json({error: "Invalid Email or password"})
+    }
+
+    //Generate JWT Token
+     const token = generateToken(user.id, res);
+
+    res.status(201).json({
+        status: "success",
+        data:{
+            user:{
+                id: user.id,
+                email: email
+            },
+            token,
+        }
+     })
+}
+
+const logout = async(req, res) =>{
+    res.cookie("jwt", "", {
+        expires: new Date(0),
+        httpOnly: true
+    })
+    res.status(200).json({
+        status: "success",
+        message: "Logged Out Successfullt"
+    });
+};
+
+export {register, login, logout};
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,11 +1,13 @@
 import express from 'express';
-import { register } from '../controllers/authController.js';
+import { register, login, logout } from '../controllers/authController.js';
 import { findUserByEmail } from "../controllers/findController.js";
 
 const router = express.Router();
 
 router.get("/find-user", findUserByEmail);
 router.post('/register', register);
+router.post('/login', login)
+router.post('/logout', logout)
 
 
 export default router;
